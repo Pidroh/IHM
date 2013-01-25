62b35a889e8e6ee158c6aaa0d935c23324baa8fd
pedro <forworksfr@hotmail.com>
2013-01-25 13:52:40 +0100

    Finished local storage for paths and HTML to process paths, somewhat

--- NetbeansProject/web/Administrateur_Client/js/backbonedefinitions.js
+++ NetbeansProject/web/Administrateur_Client/js/backbonedefinitions.js
@@ -17,13 +17,15 @@
 		defaults : {
 			name : "",
 			body : "",
+			id : 0,
 			notes: new collecNotes()
 		},
         initialize: function(){
         }
     });
 	
-	var collecPaths = Backbone.Collection.extend({
+	window.collecPaths = Backbone.Collection.extend({
+		//localStorage: new Backbone.LocalStorage("collecPathsSaving"),
 		model : Path
 	});
 	
@@ -34,21 +36,54 @@
 		this._options = [];
 		this.collection.each(this.add);	
 		this.collection.bind('add', this.add);
-		
+		this.numbered = false;		
 	  },
 	  add : function(note) {
-	  
+	  	
 		var option=document.createElement("option");
-		option.text=note.get("name");
+		option.text=this.collection.indexOf(note)+1+" - "+note.get("name");
 		this._options.push(option);
 		
 	  },
 	  render: function() {
 		$(this.el).empty();
 		var a = this.el;
+		for (var i=0; i< this._options.length; i++) {
+
+			a.add(this._options[i], null);
+		}
 		_(this._options).each(function(dv) {
-			a.add(dv, null);
+
 		});
 		
 	  }
 	});
+
+	var CollecToList = Backbone.View.extend({
+		
+	  initialize: function() {
+		_(this).bindAll('add', 'remove');
+		this._options = [];
+		this.collection.each(this.add);	
+		this.collection.bind('add', this.add);
+
+	  },
+	  add : function(note) {
+	  
+		this._options.push(note.get("name"));
+		
+	  },
+	  render: function() {
+		$(this.el).empty();
+		var a = this.el;
+		_(this._options).each(function(dv) {
+			a.innerHTML+= "<li>"+dv+"</li>";
+		});
+		
+	  }
+	});
+
+function saveCollectionToLocal(k, c){
+//	alert(JSON.stringify(c));
+	localStorage[k] = JSON.stringify(c);
+}
